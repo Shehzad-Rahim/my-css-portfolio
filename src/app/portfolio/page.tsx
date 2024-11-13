dd72aca9257e12bad538440f427623373bc4f836
Shehzad-Rahim <shehzadrahim33@gmail.com>
2024-11-13 15:47:58 +0500

    updated

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,88 +1,71 @@
-'use client';
-import React, { useState } from 'react';
-
 import Link from 'next/link'
-const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
+import Image from 'next/image'
+import project1 from '../../../public/images/project1.png'
+import project2 from '../../../public/images/project2.png'
+import project3 from '../../../public/images/project3.png'
+import project4 from '../../../public/images/project4.png'
+import project5 from '../../../public/images/project5.png'
+import project6 from '../../../public/images/project6.png'
+const page = () => {
   return (
-    <>
-    <header>
-      <div >
-          <div id='logo'>
-          <h1>SR</h1>
-          </div>
+    <div id='portfolio-container'>
+      <h1 id='portfolio-title'>PORTFOLIO</h1>
+      <p>Explore our diverse portfolio showcasing successful projects in web development, mobile app creation, and web design. Each project highlights our commitment to quality, creativity, and functionality, tailored to meet client needs and goals.</p>
+      
+      <div id='portfoli-projects-container'>
+        <div className='projects-img-container'>
+           <Image className='Image' src={project1} alt='project1'></Image>
+           <div className="project-details">
+            <h2>3d book portfolio</h2>
+            <p>This is a 3d book design portfolio website created by Shehzad Rahim</p>
+            <Link className='link' href={''}>visit site</Link>
+           </div>
         </div>
-        
-        <nav>
-            <ul >
-                <li>
-                    <Link href={"/"}>About Me</Link>
-                </li>
-                <li>
-                    <Link href={"/myservices"}>My Services</Link>
-                </li>
-                <li>
-                    <Link href={"/portfolio"}>Portfolio</Link>
-                </li>
-                <li>
-                    <Link href={"/contact"}>Contact Me</Link>
-                </li>
-            </ul>
-        </nav>
-        
-        <div id='toggle-btn'
-            onClick={toggleMenu}
-          >
-            {/* Hamburger icon */}
-            <svg
-              className={` ${isMenuOpen ? 'hidden' : 'block'}`}
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M4 6h16M4 12h16m-7 6h7" />
-            </svg>
+        <div className='projects-img-container'>
+        <Image className='Image' src={project2} alt='project1'></Image>
+        <div className="project-details">
+            <h2>Online Services</h2>
+            <p>This is an online services website created by Shehzad Rahim</p>
+            <Link className='link' href={''}>visit site</Link>
+           </div>
+        </div>
+        <div className='projects-img-container'>
+        <Image className='Image' src={project3} alt='project1'></Image>
+        <div className="project-details">
+            <h2>Figma Design</h2>
+            <p>This is a portfolio website created with figma design templates by Shehzad Rahim</p>
+            <Link className='link' href={''}>visit site</Link>
+           </div>
+        </div>
+        <div className='projects-img-container'>
+        <Image className='Image' src={project4} alt='project1'></Image>
+        <div className="project-details">
+            <h2>Resume Builder</h2>
+            <p>This is a Resume Builder website created by Shehzad Rahim</p>
+            <Link className='link' href={''}>visit site</Link>
+           </div>
+        </div>
+        <div className='projects-img-container' >
+        <Image className='Image' src={project5} alt='project1'></Image>
+        <div className="project-details">
+            <h2>Dinning on the</h2>
+            <p>This is a E-commerce food website created by Shehzad Rahim</p>
+            <Link className='link' href={''}>visit site</Link>
+           </div>
+        </div>
+        <div className='projects-img-container'>
+        <Image className='Image' src={project6} alt='project1' ></Image>
+        <div className="project-details">
+            <h2>Photography Agency</h2>
+            <p>This is a Photography Agency website created by Shehzad Rahim</p>
+            <Link className='link' href={''}>visit site</Link>
+           </div>
+        </div>
+      </div>
 
-            {/* Close icon */}
-            <svg
-              className={`${isMenuOpen ? 'block' : 'hidden'}`}
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </div>
-    </header>
-    {isMenuOpen && (
-          <div id='toggleNavbar' >
-            <Link href="/"  onClick={toggleMenu}> 
-                <h1>About</h1>
-            </Link>
-            <Link  href="/myservices" onClick={toggleMenu}>
-            <h1>My Services</h1>
-            </Link>
-            <Link  href="/portfolio" onClick={toggleMenu}>
-             <h1>Portfolio</h1>
-            </Link>
-            <Link  href="/contact" onClick={toggleMenu}>
-             <h1>Contact Me</h1>
-            </Link>
-            
-          </div>
-        )}
-    </>
+      <button>See More</button>
+    </div>
   )
 }
 
-export default Header
+export default page
