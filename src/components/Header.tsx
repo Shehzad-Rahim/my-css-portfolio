ce3592118d6c15fc52fb9c0eafb462ea26170517
Shehzad-Rahim <shehzadrahim33@gmail.com>
2024-11-13 15:42:57 +0500

    some code added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,16 +10,16 @@ const Header = () => {
 
   return (
     <>
-    <header className='flex justify-between'>
+    <header>
       <div >
-          <div id='logo' className=" z-10 lg:w-[220px] md:w-[150px] w-[120px] h-[100px] lg:h-[200px] md:h-[130px] flex justify-center items-center absolute lg:-top-20 md:-top-12 -top-9  lg:-left-5 md:-left-4 -left-3 bg-gradient-to-r from-[#e90074] to-[#660088] rounded-br-[100%] rounded-bl-[120px]">
-          <h1 className="lg:text-5xl md:text-4xl text-3xl lg:mt-12 md:mt-9 mt-5 font-bold">SR</h1>
+          <div id='logo'>
+          <h1>SR</h1>
           </div>
         </div>
         
-        <nav className='py-10 px-20 md:block hidden'>
-            <ul className='flex gap-16 text-[18px]'>
-                <li className='active'>
+        <nav>
+            <ul >
+                <li>
                     <Link href={"/"}>About Me</Link>
                 </li>
                 <li>
@@ -34,13 +34,12 @@ const Header = () => {
             </ul>
         </nav>
         
-        <button
-            className="md:hidden hover:bg-transparent hover:text-[#e90074] inline-flex items-center justify-center p-2 rounded text-white focus:outline-none"
+        <div id='toggle-btn'
             onClick={toggleMenu}
           >
             {/* Hamburger icon */}
             <svg
-              className={`z-10 w-8 h-8 ${isMenuOpen ? 'hidden' : 'block'}`}
+              className={` ${isMenuOpen ? 'hidden' : 'block'}`}
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -53,7 +52,7 @@ const Header = () => {
 
             {/* Close icon */}
             <svg
-              className={`z-10 w-8 h-8 ${isMenuOpen ? 'block' : 'hidden'}`}
+              className={`${isMenuOpen ? 'block' : 'hidden'}`}
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -63,20 +62,20 @@ const Header = () => {
             >
               <path d="M6 18L18 6M6 6l12 12" />
             </svg>
-          </button>
+          </div>
     </header>
     {isMenuOpen && (
-          <div id='toggleNavbar' className="md:hidden my-4 absolute top-0 w-full text-xl bg-[#1c1c1c] bg-opacity-[0.9]  text-white flex flex-col items-center ml-3 space-y-2 py-2">
-            <Link className='hover:text-[#e90074]' href="/"  onClick={toggleMenu}> 
+          <div id='toggleNavbar' >
+            <Link href="/"  onClick={toggleMenu}> 
                 <h1>About</h1>
             </Link>
-            <Link className='hover:text-[#e90074]' href="/myservices" onClick={toggleMenu}>
+            <Link  href="/myservices" onClick={toggleMenu}>
             <h1>My Services</h1>
             </Link>
-            <Link className='hover:text-[#e90074]' href="/portfolio" onClick={toggleMenu}>
+            <Link  href="/portfolio" onClick={toggleMenu}>
              <h1>Portfolio</h1>
             </Link>
-            <Link className='hover:text-[#e90074]' href="/contact" onClick={toggleMenu}>
+            <Link  href="/contact" onClick={toggleMenu}>
              <h1>Contact Me</h1>
             </Link>
             
